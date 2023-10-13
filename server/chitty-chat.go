13142ea641ef78be7a2ace615c360089c6ab0ec8
Phi Va Lo <phiy@itu.dk>
2023-10-13 12:19:56 +0200

    Added some old gRPC code + edit
    Co-authored-by: Patrick Søborg <ptso@itu.dk>
    Co-authored-by: Anna Høybye Johansen <annaj@itu.dk>

--- server/chitty-chat.go
+++ server/chitty-chat.go
@@ -1,10 +1,115 @@
 package main
 
 import (
-	"fmt"
+	"PhysicalTime/proto"
+	"context"
+	"flag"
+	"log"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"google.golang.org/grpc"
 )
 
+// Server Struct that will be used to represent the Server.
+type Server struct {
+	proto.UnimplementedTimeAskServer
+	name string
+	port int
+	timestamp int
+}
+
+// Used to get the user-defined port for the server from the command line
+var port = flag.Int("port", 0, "server port number")
+
 func main() {
-	fmt.Println("Hello World!")
+	// Get the port from the command line when the server is run
+	flag.Parse()
+
+	// Create a server struct
+	server := &Server{
+		name: "serverName",
+		port: *port,
+	}
+
+	// Start the server
+	go startServer(server)
+
+	// Keep the server running
+	for {
+	}
+}
+
+func startServer(server *Server) {
+	// Create a new grpc server
+	grpcServer := grpc.NewServer()
+
+	// Make the server listen at the given port (convert int port to string)
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(server.port))
+
+	// Error handling if server could not be created
+	if err != nil {
+		log.Fatalf("Could not create the server %v", err)
+	}
+	log.Printf("Started server at port: %d\n", server.port)
+
+	// Register the grpc server and serve its listener
+	proto.RegisterTimeAskServer(grpcServer, server)
+	serveError := grpcServer.Serve(listener)
+	if serveError != nil {
+		log.Fatalf("Could not serve listener")
+	}
+}
+
+
+func (server *Server) participantLeft(){
+	server.timestamp++;
+	//PRINT (Participant X left Chitty-Chat at Lamport time L)
+	log.Printf("Participant %d left Chitty-chat\n", in.ClientId)
+}
+
+func (server *Server) participantJoined(){
+	server.timestamp++;
+	//PRIT (Participant X  joined Chitty-Chat at Lamport time L)
+	log.Printf("Participant %d joined Chitty-chat\n", in.ClientId)
+}
+
+
+
+func (server *Server) clientPublishMessage(ctx context.Context, in *proto.PublishMessage) (*proto.BroadcastMessage, error){
+	//Server receives the message therefore timestamp++
+	if(server.timestamp < in.timestamp) {
+		server.timestamp = in.timestamp
+	}
+	server.timestamp++;
+
+	//PRINT (Participant x send the message: *** at lamport timestamp)
+	log.Printf("Participant %s send the message: %s at lamport timestamp %s\n", in.ClientId, in.message, server.timestamp)
 	
+	//Broadcast to the rest of the participants therefore timestamp++
+	server.timestamp++;
+
+	return &proto.BroadcastMessage {
+		timestamp:      server.timestamp,
+		message: 		in.message,
+	}, nil
+}
+
+// Der er to clients den første sender en besked (lamport = 1)
+// Server modtager besked (lamport = 2)
+// Server sender besked til første client (lamport = 3)
+// Server sender besked til anden client (lamport = 4)
+
+// Første client sender ny besked (lamport = 4)
+
+
+
+func (server *Server) AskForTime(ctx context.Context, in *proto.AskForTimeMessage) (*proto.TimeMessage, error) {
+	log.Printf("Client with ID %d asked for the time\n", in.ClientId)
+	return &proto.TimeMessage{
+		Time:       time.Now().String(),
+		ServerName: server.name,
+	}, nil
 }
