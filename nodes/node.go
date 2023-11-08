65ba81e1d294e6aafe70c4a8e451e4dde0d7fa3f
Patrick Søborg <ptso@itu.dk>
2023-11-08 12:13:58 +0100

    Added Node struct + initializing flags
    Co-authored-by: Anna Høybye Johansen <annaj@itu.dk>
    Co-authored-by: Phi Va Lo <phiy@itu.dk>

--- nodes/node.go
+++ nodes/node.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"LogicalTime/proto"
+	"flag"
+)
+
+type Node struct {
+	proto.UnimplementedNodeServer
+	id        int
+	port      int
+	timestamp int
+	nodes     map[int]int
+}
+
+var (
+	nodePort = flag.Int("port", 0, "node port number")
+	nodeID   = flag.Int("id", 0, "nodeID should be unique")
+)
+
 func main() {
 
 }
