d7a6c15cdc8f7a94acc6e300cc2f81528305c9c6
Patrick Søborg <ptso@itu.dk>
2023-11-08 12:20:46 +0100

    Added main func with node initializer
    Co-authored-by: Anna Høybye Johansen <annaj@itu.dk>
    Co-authored-by: Phi Va Lo <phiy@itu.dk>

--- nodes/node.go
+++ nodes/node.go
@@ -3,6 +3,9 @@ package main
 import (
 	"LogicalTime/proto"
 	"flag"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type Node struct {
@@ -14,10 +17,32 @@ type Node struct {
 }
 
 var (
-	nodePort = flag.Int("port", 0, "node port number")
-	nodeID   = flag.Int("id", 0, "nodeID should be unique")
+	port = flag.Int("port", 0, "node port number")
+	id   = flag.Int("id", 0, "nodeID should be unique")
 )
 
 func main() {
+	// Get the port and id from the command line when the node is initialized
+	flag.Parse()
+
+	// Create a node struct
+	node := &Node{
+		id:        *id,
+		port:      *port,
+		timestamp: 1,
+		nodes:     make(map[int]int),
+	}
+
+	// Starts node as a grpc server
+	go startNode(node)
+
+	// Keep the client running / Wait for shutdown
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM) //Notifies the channel when ctrl+c is pressed or the terminal is interrupted
+
+	<-signalChannel //Waits for the channel to receive a signal
+}
+
+func startNode(node *Node) {
 
 }
